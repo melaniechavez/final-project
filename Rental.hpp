7ebf47aee0fb99c3579ae68b73293c16391f853d
Karston Christensen <christensenkarston@gmail.com>
2019-04-19 14:51:19 -0600

    Update Rental.hpp
    Updated

--- Rental.hpp
+++ Rental.hpp
@@ -1,62 +1,64 @@
 #include <string>
 #include <cstring>
 
-struct LLRentalCar
+using namespace std;
+
+struct CarNode
 {
-  std::string make;
-  std::string color;
-  std::string dropoff;
+  string make;
+  string color;
+  string two_way;
   float price;
   int year;
   float weight;
-  struct LLRentalCar* next;   // Pointer to the next node
+  struct CarNode* next;   // Pointer to the next node
 
-  LLRentalCar(){} // default constructor
-
-  // Parametrized constructor
-  LLRentalCar(std::string m, std::string c, ::string dropoff d, float p, int y, float w) : make(r), color(c),
-      dropoff(d), price(p), year(y), weight(w), next(NULL) {}
+  CarNode(){} // default constructor
 };
-class LCar
+
+
+class CarList
 {
   private:
-    LLRentalCar* head;  // Pointer to the head.
+    CarNode* head;  // Pointer to the head.
 
   public:
-    void insertCar(std::string Nmake, std::string Ncolor, std::string Ndropoff, float Nprice, int Nyear);
+    CarList();
+    bool isEmpty();
+    void insertCar(string make, string color, string two_way, float price, int year, float weight);
 
-    LLRentalCar* searchCar(std::string Nmake);
-    // Searches for the car if it's in the linked list.
+    void printList();
 
-    weighting_function(LLRentalCar* node);
-    // Calculates weight of the car.
+    void weighting_function();
+
+    CarNode* getHead();
 };
-class RentalCar
+
+class RentalCarQueue
 {
     public:
 
     //Purpose: perform all operations necessary to instantiate a class object
-    RentalCar(int queueSize);
+    RentalCarQueue(int queueSize);
 
     // Purpose: free all resources that the object has acquired
-    ~RentalCar();
+    ~RentalCarQueue();
 
     // Purpose: enqueue new group into priority queue; call other
-    void enqueue (std::string _make, int _year, ::string _model, float _price);
-
-    //Purpose: remove the group (allocate the table) at the front of the priority queue from
-    void dequeue();
+    void enqueue(CarNode node);
 
     // return: groupID of the group at the front of the priority queue
-    LLRentalCar peek();
+    CarNode peek();
 
-    void printL();
+    void printQueue();
 
     //return: true if queue is full, false otherwise
     bool isFull();
 
     // return: true if queue is empty, false otherwise
     bool isEmpty();
+    //
+    void dequeue();
 
     private:
     //Purpose: maintain heap properties by swapping node with parent if necessary
@@ -65,11 +67,10 @@ class RentalCar
     // Purpose: maintain heap properties by swapping node with child if necessary
     void repairDownward(int nodeIndex);
 
-    void heapSort(int arr1[], int arr2[]);
+    // void heapSort(int arr1[], int len);
 
-    // Sorts the array at a given list
-
-    LLRentalCar* priorityQueue; //pointer to the array used to implement priority queue
+    CarNode* priorityQueue; //pointer to the array used to implement priority queue
     int currentQueueSize; //number of group currently in the priority queue
     int maxQueueSize; //maximum capacity of priority queue
 };
+
