7d4c0f330b99c052a74035aed84d0d1952a7d494
Karston Christensen <christensenkarston@gmail.com>
2019-04-22 15:52:23 -0600

    Update Rental.hpp

--- Rental.hpp
+++ Rental.hpp
@@ -3,37 +3,53 @@
 
 using namespace std;
 
+
+// Create struct for nodes containing information about rental cars
 struct CarNode
 {
+  // make and model of the car
   string make;
+  // color of the car
   string color;
+  // can the car be dropped off as a two way or one way rental?
   string two_way;
+  // price of the rental
   float price;
+  // year of the car
   int year;
+  // a weighting parameter to be used when sorting the rental options
   float weight;
-  struct CarNode* next;   // Pointer to the next node
-
-  CarNode(){} // default constructor
+  // pointer to the next CarNode in the linked lsit
+  struct CarNode* next;
+  // constructor
+  CarNode(){}
 };
 
 
+// Create a linked list data structure to store CarNodes
 class CarList
 {
   private:
-    CarNode* head;  // Pointer to the head.
+    // pointer to the head of the linked list
+    CarNode* head;
 
   public:
+    // constructor
     CarList();
+    // is the list empty?
     bool isEmpty();
+    // insert a CarNode to the linked list based on its parameters
     void insertCar(string make, string color, string two_way, float price, int year, float weight);
-
+    // print the linked list
     void printList();
-
+    // assigns a weight to each CarNode based on user prompted priorities
     void weighting_function();
-
+    // returns the head of the linked list
     CarNode* getHead();
 };
 
+
+// Create Priority Queue implemented as a Binary Heap
 class RentalCarQueue
 {
     public:
@@ -50,6 +66,7 @@ class RentalCarQueue
     // return: groupID of the group at the front of the priority queue
     CarNode peek();
 
+    // print the contents of the queue
     void printQueue();
 
     //return: true if queue is full, false otherwise
@@ -57,13 +74,14 @@ class RentalCarQueue
 
     // return: true if queue is empty, false otherwise
     bool isEmpty();
-    //
+
+    // remove, or pop, the item off the top of the queue
     CarNode dequeue();
 
-    //CarNode* heapSort();
-
+    // return the pointer to the priority queue array
     CarNode* return_arr();
 
+    // return the current quantity of items in the priority queue
     int return_len();
 
     private:
@@ -73,10 +91,7 @@ class RentalCarQueue
     // Purpose: maintain heap properties by swapping node with child if necessary
     void repairDownward(int nodeIndex);
 
-    // void heapSort(int arr1[], int len);
-
     CarNode* priorityQueue; //pointer to the array used to implement priority queue
     int currentQueueSize; //number of group currently in the priority queue
     int maxQueueSize; //maximum capacity of priority queue
 };
-
