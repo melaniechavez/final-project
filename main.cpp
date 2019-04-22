e4feced54941f460b11029a02a79c60264fe4c23
Karston Christensen <christensenkarston@gmail.com>
2019-04-22 15:53:47 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include <string>
 using namespace std;
 
-
+// Heapsort function to sort the items of the priority queue.
+// Will be used to sort the queue based on weight and price.
 CarNode* heapSort(int len, CarNode arr[]){
   RentalCarQueue hp(len);
   for(int i=0; i<len; i++){
@@ -17,14 +18,15 @@ CarNode* heapSort(int len, CarNode arr[]){
   return arr;
 }
 
+// MAIN function that executes a rental ranking
 int main(int argc, char* argv[]) {
 
   string filename = argv[1];
 
-
+  // declare a linked list of CarNodes
   CarList l;
 
-  // open the text file
+  // open the input rental car text file
   ifstream myFile;
   myFile.open(argv[1]);
   string make;
@@ -49,6 +51,7 @@ int main(int argc, char* argv[]) {
       int year = stoi(year_);
       float price = stof(price_);
 
+      // insert a CarNode to the linked list with values from the input file
       l.insertCar(make,color,dropoff,price,year,0);
     }
   }
@@ -60,6 +63,8 @@ int main(int argc, char* argv[]) {
   myFile.close();
 
 
+  // find the length of the linked list for testing purposes.
+  // This portion of the code does not have any significance in regards to the objective
   int length = 0;
   CarNode* temp = l.getHead();
   while(temp != nullptr){
@@ -67,9 +72,11 @@ int main(int argc, char* argv[]) {
     temp = temp->next;
   }
 
+
+  // call the weighting function to sort the options into relative rankings
   l.weighting_function();
 
-
+  // if the user specifies that they desire a two-way rental, all one-ways will be put in a "non-viable" list
   CarList lreject;
   int reject_count = 0;
   temp = l.getHead();
@@ -81,6 +88,8 @@ int main(int argc, char* argv[]) {
     temp = temp->next;
   }
 
+  // Begin the process of adding the CarNodes into a priority queue based on weight and price.
+  // Enqueue the cars that will work based on the two-way or one-way specification
   temp = l.getHead();
   RentalCarQueue cq(length-reject_count);
   while(temp != nullptr){
@@ -90,30 +99,47 @@ int main(int argc, char* argv[]) {
     temp = temp->next;
   }
 
+  // sort the priority queue using a heapsort
   int list_length = cq.return_len();
   CarNode* arr = cq.return_arr();
   arr = heapSort(list_length,arr);
 
-  cout << "Sorted List" << endl;
+  // print the sorted list to the terminal
+  cout << "Sorted List: " << endl;
   for(int i=0; i<list_length; i++){
-    cout<< i+1 << ". " << arr[i].make << ","<< arr[i].color  << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
+    cout<< i+1 << ". " << arr[i].make << ", "<< arr[i].color  << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
+  }
+  cout << endl;
+  cout << "Rental Options That Will Not Work Based on Two-Way Specification: " << endl;
+  temp = lreject.getHead();
+  while(temp != NULL){
+    cout << temp->make << ", " << temp->color << ", " << temp->year << ", "<< temp->price << ", "<< endl;
+    temp = temp->next;
   }
 
-  // After the user's preference, we write their information into a separate txt file.
-  
+  // write the results of the sorting process to an output .txt file
   ofstream outFile;
   outFile.open ("Results.txt");
-  outFile << "Ranked Results\n";
+  outFile << "Ranked Results: ";
   outFile << endl;
-  outFile << "Top 5 Options"<< endl;
+  outFile << "Top 5 Options: "<< endl;
   for(int i=0; i<5; i++){
-    outFile << i+1 << ". " << arr[i].make << ","<< arr[i].color << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
+    outFile << i+1 << ". " << arr[i].make << ", "<< arr[i].color << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
   }
   outFile << endl;
-  outFile << "Other Ranked Options" << endl;
+  outFile << "Other Ranked Options: " << endl;
   for(int i=5; i<list_length; i++){
-    outFile << i+1 << ". " << arr[i].make << ","<< arr[i].color << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
+    outFile << i+1 << ". " << arr[i].make << ", "<< arr[i].color << ", "<< arr[i].year <<", $"<< arr[i].price<<endl;
   }
+  outFile << endl;
+  outFile << "Rental Options That Will Not Work Based on Two-Way Specification: " << endl;
+  temp = lreject.getHead();
+  while(temp != NULL){
+    outFile << temp->make << ", " << temp->color << ", " << temp->year << ", "<< temp->price << ", "<< endl;
+    temp = temp->next;
+  }
+
+
   outFile.close();
 
   return 0;
