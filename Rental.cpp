2b1cbb75671e96ef33f05ef2e87f6df3caed7d46
Karston Christensen <christensenkarston@gmail.com>
2019-04-22 15:53:09 -0600

    Update Rental.cpp

--- Rental.cpp
+++ Rental.cpp
@@ -6,12 +6,13 @@
 
 using namespace std;
 
-
+// Constructor of the linked list
 CarList::CarList()
 {
   head = nullptr;
 }
 
+// checks if the list is empty
 bool CarList::isEmpty()
 {
   if(head == nullptr){
@@ -22,6 +23,7 @@ bool CarList::isEmpty()
   }
 }
 
+// inserts a car into the linked list by creating a CarNode out of the passed information
 void CarList::insertCar(string make, string color, string two_way, float price, int year, float weight)
 {
   CarNode* temp = new CarNode;
@@ -35,10 +37,12 @@ void CarList::insertCar(string make, string color, string two_way, float price, 
   head = temp;
 }
 
+// returns the head of the linked list
 CarNode* CarList::getHead(){
   return head;
 }
 
+// prints the linked list
 void CarList::printList()
 {
   CarNode *current = head;
@@ -57,12 +61,16 @@ void CarList::printList()
   }
 }
 
-
+// assigns a weight based on user prompts
 void CarList::weighting_function(){
 
-  int option = 0;
-  int state = 1;
-  int redo = 0;
+  // declare all necessary variables and set defaults as necessary
+  // set state machine parameters
+  int option = 0; // "option" determines whether the user is being prompted or not
+  int state = 1; // "state" determines which question the user is being prompted by
+  int redo = 0; // if the user needs to return to change a field, this variable will Challenger
+
+  // important parameters to be used in calculating weights later
   string make_yn;
   string make_importance = "1";
   string make;
@@ -78,29 +86,31 @@ void CarList::weighting_function(){
   string two_way_yn;
   string state_sel;
 
+  // User Prompting Section, Beginning of State Machine
   while(option == 0){
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Prompt the user for information about make and model
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(state == 1 && option == 0){
-      cout << "Is a certain make desired? (Enter Y/N):" << endl;//<< endl;
+      cout << "Is a certain make desired? (Enter Y/N):" << endl;
       cout << "#> ";
       cin >> make_yn;
       while(make_yn != "Y" && make_yn != "N"){
-        cout << "Is a certain make desired? Please enter Y or N as capital letters:" << endl;//<< endl;
+        cout << "Is a certain make desired? Please enter Y or N as capital letters:" << endl;
         cout << "#> ";
         cin >> make_yn;
       }
 
       if(make_yn == "Y"){
-        cout << "Enter the make and model: (E.g. Toyota Camry)" << endl;//<< endl;
+        cout << "Enter the make and model: (E.g. Toyota Camry)" << endl;
         cout << "#> ";
         cin.ignore();
         getline(cin,make);
-        //cout << "make: "  << make << endl;
-        cout << "Rank the importance of make from 0 to 5, with 5 being most important:" << endl;//<< endl;
+        cout << "Rank the importance of make from 0 to 5, with 5 being most important:" << endl;
         cout << "#> ";
         cin >> make_importance;
         while(make_importance != "0" && make_importance !="1" && make_importance !="2" && make_importance !="3" && make_importance !="4"&& make_importance !="5"){
-          cout << "Please enter the importance as a number between 0 and 5:" << endl;//<< endl;
+          cout << "Please enter the importance as a number between 0 and 5:" << endl;
           cout << "#> ";
           cin >> make_importance;
         }
@@ -113,31 +123,29 @@ void CarList::weighting_function(){
       }
     }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Prompt the user for information about the color
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(state == 2 && option == 0){
-      cout << "Is a certain color desired? (Enter Y/N):" << endl;//<< endl;
+      cout << "Is a certain color desired? (Enter Y/N):" << endl;
       cout << "#> ";
       cin >> color_yn;
       while(color_yn != "Y" && color_yn != "N"){
-        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;//<< endl;
+        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;
         cout << "#> ";
         cin >> color_yn;
       }
-      //cout << "color_yn: " << color_yn << endl;
       if(color_yn == "Y"){
-        cout << "List the desired color of the car: (E.g. Green)" << endl;//<< endl;
+        cout << "List the desired color of the car: (E.g. Green)" << endl;
         cout << "#> ";
         cin >> color;
-        //cout << "color: " << color << endl;
-        cout << "Rank the importance of color from 0 to 5, with 5 being most important:" << endl;//<< endl;
+        cout << "Rank the importance of color from 0 to 5, with 5 being most important:" << endl;
         cout << "#> ";
         cin >> color_importance;
         while(color_importance != "0" && color_importance !="1" && color_importance !="2" && color_importance !="3" && color_importance !="4"&& color_importance !="5"){
-          cout << "Please enter the importance as a number between 0 and 5:" << endl;//<< endl;
+          cout << "Please enter the importance as a number between 0 and 5:" << endl;
           cout << "#> ";
           cin >> color_importance;
         }
-        //cout << "color_importance: " << color_importance << endl;
       }
       if(redo == 0){
         state = 3;
@@ -147,31 +155,29 @@ void CarList::weighting_function(){
       }
     }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Prompt the user for information about the price
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(state == 3 && option == 0){
-      cout << "Is the price a determining factor? (Enter Y/N):" << endl;//<< endl;
+      cout << "Is the price a determining factor? (Enter Y/N):" << endl;
       cout << "#> ";
       cin >> price_yn;
       while(price_yn != "Y" && price_yn != "N"){
-        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;//<< endl;
+        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;
         cout << "#> ";
         cin >> price_yn;
       }
-      //cout << "price_yn: " << price_yn << endl;
       if(price_yn == "Y"){
-        cout << "List the desired price of the car:" << endl;//<< endl;
+        cout << "List the desired price of the car:" << endl;
         cout << "#> ";
         cin >> price;
-        //cout << "price: " << price << endl;
-        cout << "Rank the importance of price from 0 to 5, with 5 being most important:" << endl;//<< endl;
+        cout << "Rank the importance of price from 0 to 5, with 5 being most important:" << endl;
         cout << "#> ";
         cin >> price_importance;
         while(price_importance != "0" && price_importance !="1" && price_importance !="2" && price_importance !="3" && price_importance !="4"&& price_importance !="5"){
-          cout << "Please enter the importance as a number between 0 and 5:" << endl;//<< endl;
+          cout << "Please enter the importance as a number between 0 and 5:" << endl;
           cout << "#> ";
           cin >> price_importance;
         }
-        //cout << "price_importance: " << price_importance << endl;
       }
       if(redo == 0){
         state = 4;
@@ -181,31 +187,29 @@ void CarList::weighting_function(){
       }
     }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Prompt the user for information about the year
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(state == 4 && option == 0){
-      cout << "Is a certain year desired? (Enter Y/N):" << endl;//<< endl;
+      cout << "Is a certain year desired? (Enter Y/N):" << endl;
       cout << "#> ";
       cin >> year_yn;
       while(year_yn != "Y" && year_yn != "N"){
-        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;//<< endl;
+        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;
         cout << "#> ";
         cin >> year_yn;
       }
-      //cout << "year_yn: " << year_yn << endl;
       if(year_yn == "Y"){
-        cout << "List the desired year of the car:" << endl;//<< endl;
+        cout << "List the desired year of the car:" << endl;
         cout << "#> ";
         cin >> year;
-        //cout << "year: " << year << endl;
-        cout << "Rank the importance of year from 0 to 5, with 5 being most important:" << endl;//<< endl;
+        cout << "Rank the importance of year from 0 to 5, with 5 being most important:" << endl;
         cout << "#> ";
         cin >> year_importance;
         while(year_importance != "0" && year_importance !="1" && year_importance !="2" && year_importance !="3" && year_importance !="4"&& year_importance !="5"){
-          cout << "Please enter the importance as a number between 0 and 5:" << endl;//<< endl;
+          cout << "Please enter the importance as a number between 0 and 5:" << endl;
           cout << "#> ";
           cin >> year_importance;
         }
-        //cout << "year_importance: " << year_importance << endl;
       }
       if(redo == 0){
         state = 5;
@@ -215,21 +219,22 @@ void CarList::weighting_function(){
       }
     }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Prompt the user for information about the car's dropoff protocol
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if(state == 5 && option == 0){
-      cout << "Is it necessary for the rental to be two_way? (Enter Y/N):" << endl;//<< endl;
+      cout << "Is it necessary for the rental to be two_way? (Enter Y/N):" << endl;
       cout << "#> ";
       cin >> two_way_yn;
       while(two_way_yn != "Y" && two_way_yn != "N"){
-        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;//<< endl;
+        cout << "Is a certain color desired? Please enter Y or N as capital letters:" << endl;
         cout << "#> ";
         cin >> two_way_yn;
       }
-      //cout << "two_way_yn: " << two_way_yn << endl;
       option = 1;
     }
 
 
+    // Make sure that the user is satisfied with their query
     cout << "You have selected: " << endl;
     if(make_yn == "Y"){
       cout << "Make and Model: " << make << endl;
@@ -267,6 +272,8 @@ void CarList::weighting_function(){
       cout << "#> ";
       cin >> final_yn;
     }
+
+    // If the user is not satisfied with their query, return to the user prompting section by reassigning state machine variables
     if(final_yn == "N"){
       option = 0;
       redo = 1;
@@ -288,41 +295,54 @@ void CarList::weighting_function(){
     }
   }
 
+
+  // Begin the mathematical weighting section
   int make_weight;
   int color_weight;
   int price_weight;
   int year_weight;
 
+  // initialize arrays necessary for calculating the weights
   string yn[4] = {make_yn ,color_yn, price_yn, year_yn};
   int importance[4] = {stoi(make_importance), stoi(color_importance), stoi(price_importance), stoi(year_importance)};
   int weights[4] = {make_weight, color_weight, price_weight, year_weight};
 
 
+  // iterate over the linked list and calculate a weight for each car based on user answers
   CarNode* tmp = head;
 
   while(tmp != nullptr){
+    // if the car does not meet the two-way requirements, assign a weight of -1 for sorting purposes
     if(two_way_yn == "Y" && tmp->two_way == "one-way"){
       tmp->weight = -1;
     }
     else{
 
+      // assign the fulfilled category to unity by default
       float fulfilled[4] = {1,1,1,1};
 
+      // for make and model, if the user specifies a certain model and it is not fulfilled, assign fulfilled as 1.
+      // Otherwise, assign fulfilled as 6
       if(tmp->make != make && make_yn == "Y"){
         fulfilled[0] = 1;
       }
       else{
         fulfilled[0] = 6;
       }
+
+      // Same protocol for color
       if(tmp->color != color && color_yn == "Y"){
         fulfilled[1] = 1;
       }
       else{
         fulfilled[1] = 6;
       }
+
+      // If price is not specified, assign fulfilled as 5.
       if(price_yn == "N"){
         fulfilled[2] = 5;
       }
+      // If price is specified, assign fulfilled as inversely proportional to the difference between the specified price and the car's price.
       else if(tmp->price+15 < stof(price)){
         fulfilled[2] = 4/(0.1*(stof(price)-tmp->price)+1);
       }
@@ -332,6 +352,9 @@ void CarList::weighting_function(){
       else{
         fulfilled[2] = 4*(0.1*(stof(price)-tmp->price)+1);
       }
+
+      // follow the same inverse proportionality model for the year, except assign more weight to the difference as difference in years is smaller than
+      // difference in price.
       if(year_yn == "N"){
         fulfilled[3] = 5;
       }
@@ -342,7 +365,7 @@ void CarList::weighting_function(){
         fulfilled[3] = 5*(0.1*(stoi(year)-tmp->year)+1);
       }
 
-      // calculate the weights for each criterion with an inner product of the importance of that criterion and whether what is
+      // calculate the weights for each criterion with an inner product (dot product) of the importance of that criterion and whether what is
       // desired is fulfilled
       for(int i=0; i<4; i++){
         weights[i] = importance[i]*fulfilled[i];
@@ -360,6 +383,7 @@ void CarList::weighting_function(){
 }
 
 
+// constructor for the priority queue
 RentalCarQueue::RentalCarQueue(int maxQueueSize)
 {
     currentQueueSize = 0;
@@ -367,12 +391,13 @@ RentalCarQueue::RentalCarQueue(int maxQueueSize)
     priorityQueue = new CarNode[maxQueueSize];
 }
 
-
+// deconstructor for the priority queue
 RentalCarQueue::~RentalCarQueue()
 {
     delete [] priorityQueue;
 }
 
+// is the priority queue empty?
 bool RentalCarQueue::isEmpty(){
   if(currentQueueSize == 0)
     return true;
@@ -380,6 +405,7 @@ bool RentalCarQueue::isEmpty(){
     return false;
 }
 
+// is the priority queue full?
 bool RentalCarQueue::isFull(){
   if(currentQueueSize == maxQueueSize)
     return true;
@@ -387,6 +413,7 @@ bool RentalCarQueue::isFull(){
     return false;
 }
 
+// return the CarNode at the top of the priority queue
 CarNode RentalCarQueue::peek(){
   if(!isEmpty())
     return priorityQueue[0];
@@ -395,12 +422,12 @@ CarNode RentalCarQueue::peek(){
     return priorityQueue[0];
 }
 
-
+// useful functions for determining indices of nodes in the priority queue
 int parent(int i) { return (i-1)/2; }
 int left(int i) { return (2*i+1); }
 int right(int i) { return (2*i + 2); }
 
-
+// return the priority queue to ordered state after a node is added
 void RentalCarQueue::repairUpward(int i){
   if(priorityQueue[i].weight == priorityQueue[parent(i)].weight){
     if(priorityQueue[i].price < priorityQueue[parent(i)].price){
@@ -414,7 +441,7 @@ void RentalCarQueue::repairUpward(int i){
   }
 }
 
-
+// add a node to the priority queue
 void RentalCarQueue::enqueue(CarNode node)
 {
   if(currentQueueSize == maxQueueSize){
@@ -428,6 +455,7 @@ void RentalCarQueue::enqueue(CarNode node)
   }
 }
 
+// return the queue to ordered state after removing the top of the queue
 void RentalCarQueue::repairDownward(int i)
 {
   int leftChild = left(i);
@@ -460,6 +488,7 @@ void RentalCarQueue::repairDownward(int i)
   }
 }
 
+// remove the top of the queue and return the CarNode that was removed
 CarNode RentalCarQueue::dequeue() // extractMin
 {
     if(currentQueueSize <= 0){
@@ -476,6 +505,7 @@ CarNode RentalCarQueue::dequeue() // extractMin
     return temp;
 }
 
+// generic swap function
 void swap(CarNode &x, CarNode &y)
 {
     CarNode temp = x;
@@ -483,6 +513,7 @@ void swap(CarNode &x, CarNode &y)
     y = temp;
 }
 
+// print the contents of the queue
 void RentalCarQueue::printQueue() {
   cout << "Printing Queue" << endl;
   for(int i = 0; i<currentQueueSize; i++){
@@ -490,11 +521,12 @@ void RentalCarQueue::printQueue() {
   }
 }
 
-
+// return the pointer to the array storing the nodes of the queue
 CarNode* RentalCarQueue::return_arr(){
   return priorityQueue;
 }
 
+// return the number of items in the queue
 int RentalCarQueue::return_len(){
   return currentQueueSize;
 }
